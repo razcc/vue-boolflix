0487b195b60f5593896459ba9766c6b070c268eb
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-11-29 15:48:43 +0100

    1° tentativo richiamo axios

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="app">
-    <HeaderComp @emitQuery="emitQueryFunction"/>
-    <MainComp />
-  </div>
+	<div id="app">
+		<HeaderComp @emitQuery="emitQueryFunction" />
+		<MainComp />
+	</div>
 </template>
 
 <script>
 import HeaderComp from './components/HeaderComp.vue'
 import MainComp from './components/MainComp.vue'
+import axios from "axios";
 
 
 export default {
@@ -15,6 +16,8 @@ export default {
   data(){
     return{
       valoreRicerca: "",
+      urlParziale: "https://api.themoviedb.org/3/search/movie?api_key=ea284f453f6bf939a56b81b5d0741a04&query=ritorno",
+      api: [],
     }
   },
 	components: {
@@ -26,16 +29,25 @@ export default {
       this.valoreRicerca = emitQueryValore
     },
   },
+  mounted() {
+		axios
+			.get('https://api.themoviedb.org/3/search/movie?api_key=ea284f453f6bf939a56b81b5d0741a04&query=${valoreRicerca}')
+			.then((response) => {
+        console.log(response)
+				this.api = response.data.results;			
+			});
+
+	},
 };
 </script>
 
 <style lang="scss">
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
-#app{
-  height: 100vh;
+#app {
+	height: 100vh;
 }
 </style>
