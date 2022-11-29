f0d44a05127dbfaaa35140898a27b6d0fcb6863f
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-11-29 11:55:34 +0100

    Input Header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,11 +1,27 @@
 <template>
-	<header></header>
+	<header>
+		<img
+			src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
+			alt=""
+		/>
+
+		<!-- Search -->
+		<div class="btn-group dropstart search">
+			<font-awesome-icon icon="fa-solid fa-magnifying-glass dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"/>
+			
+            <input type="text" class="dropdown-menu" placeholder="Cerca il tuo Film-Serie">
+			
+		</div>
+	</header>
 </template>
 
 <script>
 export default {
 	name: "HeaderComp",
 	components: {},
+	data() {
+		return {};
+	},
 };
 </script>
 
@@ -18,5 +34,22 @@ header {
 		rgba(80, 80, 80, 1) 0%,
 		rgba(0, 0, 0, 1) 100%
 	);
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 20px;
+}
+img {
+	height: 50%;
+}
+.fa-magnifying-glass {
+	color: white;
+    font-size: 30px;
+    margin-right: 50px;
+    margin-left: 50px;
+}
+.dropdown-menu{
+    min-width: 20rem;
+    border-color: red;
 }
 </style>
